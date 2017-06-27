4843f14ad61eeab1f616a34d0e8529ce5aaa37ee
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-06-27 22:30:39 +0300

    Setup splash screen

---
+++ Assets/Scripts/SplashScreenController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenController : MonoBehaviour {
+
+	public AudioClip introMusic;
+
+	void Start () {
+		AudioSource.PlayClipAtPoint(introMusic, transform.position);
+		Invoke("LoadStartScreen", 5);
+	}
+	
+	void LoadStartScreen()
+	{
+		GameObject.FindObjectOfType<LevelManager>().LoadNextLevel();
+	}
+}
