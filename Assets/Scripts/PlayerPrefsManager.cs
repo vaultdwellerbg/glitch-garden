108c5c4934fcec72969ca7c9a42c60ec2cce01c8
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-07-01 22:20:07 +0300

    Add PlayerPrefs container

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+
+	public static void SetMasterVolume(float volume)
+	{
+		if (volume > 0f && volume < 1f)
+		{
+			PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+		}
+		else
+		{
+			Debug.LogError("Volume should be between 0 and 1");
+		}
+	}
+	
+	public static float GetMasterVolume()
+	{
+		return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+	}
+}
