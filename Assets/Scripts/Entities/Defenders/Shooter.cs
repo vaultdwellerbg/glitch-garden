bdb1950dfb08e2896096bcf232910ea6b83a8ffe
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-09-05 19:33:58 +0300

    Fix gnome fire rate via animation

--- Assets/Scripts/Entities/Defenders/Shooter.cs
+++ Assets/Scripts/Entities/Defenders/Shooter.cs
@@ -4,31 +4,17 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
 	public GameObject projectile, projectileParent;
-	private bool reloading = false;
-	private float firingRate = 2f;
-	private float elapsedTime;
 	
 	private void Fire()
-	{
-		if (reloading) return;		
-		CreateNewProjectile ();
-		reloading = true;
-	}
-
-	void CreateNewProjectile ()
-	{
+	{	
 		GameObject newProjectile = Instantiate (projectile) as GameObject;
-		newProjectile.transform.SetParent (projectileParent.transform);
-		newProjectile.transform.position = transform.FindChild("Gun").transform.position;
+		PositionProjectile(newProjectile);
 	}
 	
-	void Update()
+	private void PositionProjectile(GameObject projectile)
 	{
-		elapsedTime += Time.deltaTime;
-		if (elapsedTime >= firingRate)
-		{
-			elapsedTime = 0;
-			reloading = false;
-		}
+		projectile.transform.SetParent (projectileParent.transform);
+		projectile.transform.position = transform.FindChild("Gun").transform.position;	
 	}
+
 }
