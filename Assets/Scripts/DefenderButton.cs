ae2ba6711743d2757a03b0981237a201986ee343
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-09-07 19:15:17 +0300

    Add defender buttons panel

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderButton : MonoBehaviour {
+
+	public static GameObject selectedDefender;
+
+	private DefenderButton[] buttons;
+	
+	void Start()
+	{
+		buttons = GameObject.FindObjectsOfType<DefenderButton>();
+	}
+
+	void OnMouseDown()
+	{
+		UnselectDefenders();
+		SelectDefender();
+	}
+
+	void UnselectDefenders()
+	{
+		foreach (var button in buttons) {
+			button.GetComponent<SpriteRenderer>().color = Color.black;
+		}
+	}
+
+	void SelectDefender()
+	{
+		GetComponent<SpriteRenderer>().color = Color.white;
+		selectedDefender = GameObject.Find(gameObject.name);
+	}
+}
