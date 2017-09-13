ff2564852a6b534e31b82f6f899c799bab056523
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-09-13 19:05:48 +0300

    Add attackers detection for shooters

--- Assets/Scripts/Entities/Defenders/Shooter.cs
+++ Assets/Scripts/Entities/Defenders/Shooter.cs
@@ -6,16 +6,71 @@ public class Shooter : MonoBehaviour {
 	public GameObject projectile;
 	
 	private GameObject projectileParent;
+	private Animator animator;
+	private Spawner currentLaneAttackersSpawner;
 	private const string PROJECTILES_PARENT_NAME = "Projectiles";
 			
 	private void Start()
 	{
+		InitializeProjectileParent();	
+		SetCurrentLaneAttackersSpawner();
+		animator = GetComponent<Animator>();
+	}
+
+	void InitializeProjectileParent ()
+	{
 		projectileParent = GameObject.Find(PROJECTILES_PARENT_NAME);
-		
-		if(!projectileParent)
-		{
-			projectileParent = new GameObject (PROJECTILES_PARENT_NAME);
+		if (!projectileParent) {
+			projectileParent = new GameObject(PROJECTILES_PARENT_NAME);
 		}
+	}
+	
+	private void SetCurrentLaneAttackersSpawner()
+	{
+		currentLaneAttackersSpawner = FindCurrentLaneAttackersSpawner();
+		if (!currentLaneAttackersSpawner) 
+		{
+			Debug.LogWarning("No attackers spawner on lane.");
+		}
+		
+	}
+
+	private Spawner FindCurrentLaneAttackersSpawner()
+	{
+		GameObject spawnersContainer = GameObject.Find("Spawners");
+		foreach (Transform child in spawnersContainer.transform) {
+			Spawner spawner = child.GetComponent<Spawner>();
+			if (spawner.transform.position.y == transform.position.y) {
+				return spawner;
+			}
+		}
+		return null;
+	}
+	
+	void Update()
+	{
+		animator.SetBool("isAttacking", IsAttackerApproaching());
+	}
+	
+	private bool IsAttackerApproaching()
+	{
+		return LineHasAttackers() && LineHasAttackerBeforeDefender();
+	}
+	
+	private bool LineHasAttackers()
+	{
+		return currentLaneAttackersSpawner.transform.childCount > 0;
+	}	
+	
+	private bool LineHasAttackerBeforeDefender()
+	{
+		foreach (Transform attackerObject in currentLaneAttackersSpawner.transform) {
+			if (attackerObject.transform.position.x > transform.position.x)
+			{
+				return true;
+			}
+		}	
+		return false;
 	}
 	
 	private void Fire()
