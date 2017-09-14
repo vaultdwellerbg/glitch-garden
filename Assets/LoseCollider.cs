66b86ebc911c7e311f2ffa61df30ab5090af23de
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-09-14 17:41:09 +0300

    Add lose condition

---
+++ Assets/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+	
+	void Start()
+	{
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+	void OnTriggerEnter2D()
+	{
+		levelManager.LoadLevel("Lose");
+	}
+}
