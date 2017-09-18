0f7758a4026b1ee770264e1e615ed0e313f62bc2
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-09-18 19:40:10 +0300

    Trigger stone attacked animation

--- Assets/Scripts/Entities/Defenders/Gravestone.cs
+++ Assets/Scripts/Entities/Defenders/Gravestone.cs
@@ -3,4 +3,12 @@ using System.Collections;
 
 public class Gravestone : MonoBehaviour {
 
+	void OnTriggerStay2D(Collider2D collider)
+	{
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		if (attacker)
+		{
+			GetComponent<Animator>().SetTrigger("isHitTrigger");
+		}
+	}
 }
